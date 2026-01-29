32983b04e25f0ad353973f055976455e27c2144d
Bimai6 <bimaivfx@gmail.com>
2026-01-29 16:23:59 +0100

    feat: add new spritesheets for stars, animation via script for stars, and tween method to improve laser animation

--- scenes/laser.gd
+++ scenes/laser.gd
@@ -2,5 +2,9 @@ extends Area2D
 
 @export var speed = 800
 
+func _ready() -> void:
+	var tween = create_tween()
+	tween.tween_property($LaserSprite, "scale", Vector2(1,1), 0.2).from(Vector2(0,0))
+
 func _process(delta: float) -> void:
 	position.y -= speed * delta
